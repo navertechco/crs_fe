147a1b323fb97e3e90303b471bc88e95e652ff73
José Cuevas NL <naver.developer@gmail.com>
2021-11-05 17:39:06 -0500

    partial profile

--- lib/common/widgets/profile_layout.dart
+++ lib/common/widgets/profile_layout.dart
@@ -3,27 +3,28 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class ProfileLayoutWidget extends StatelessWidget {
-  const ProfileLayoutWidget({Key? key, this.child, this.text}) : super(key: key);
+  const ProfileLayoutWidget({Key? key, this.child, this.text})
+      : super(key: key);
   final dynamic child;
   final String? text;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(children: [
-        Padding(
-          padding: EdgeInsets.only(bottom: Get.height * 0.81),
-          child: SvgPicture.asset(
-            "assets/custom/svg/images/Header.svg",
-            fit: BoxFit.fitWidth,
-            height: Get.height,
-            width: Get.width,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 100.0),
-          child: child,
-        ),
-      ]),
-    );
+    return Stack(fit: StackFit.expand, children: [
+      Positioned(
+        top: 0,
+        left: -1,
+        child: SvgPicture.asset(
+            "assets/custom/svg/images/gold_black_rectangle.svg",
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: MediaQuery.of(context).size.width),
+      ),
+      // Padding(
+      //   padding: const EdgeInsets.only(top: 100.0),
+      //   child: Positioned(
+      //     child: child,
+      //   ),
+      // ),
+    ]);
   }
 }
