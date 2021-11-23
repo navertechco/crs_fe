53dda66d5202a13415af89adb1c1b725ee116919
José Cuevas NL <naver.developer@gmail.com>
2021-11-23 16:24:49 -0500

    fix resume height

--- lib/common/widgets/content_layout.dart
+++ lib/common/widgets/content_layout.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-
-import '../constants.dart';
 import '../index.dart';
 
 class ContentLayoutWidget extends StatelessWidget {
-  ContentLayoutWidget({Key? key, this.child, this.text, this.background})
+  ContentLayoutWidget(
+      {Key? key, this.child, this.text, this.background, this.height = 1})
       : super(key: key);
   final dynamic child;
   final String? text;
+  final double height;
   String? background;
   @override
   Widget build(BuildContext context) {
-    var icons = getContext("icons");
     var multiplier = MediaQuery.of(context).orientation == Orientation.landscape
         ? 0.1
         : 0.09;
-    String icon = icons[text] ?? "Default";
     return Stack(children: [
-      Image.asset(
-        background ?? "assets/custom/img/layout.png",
-        fit: BoxFit.fill,
-        height: double.infinity,
-        width: double.infinity,
+      Expanded(
+        flex: 1,
+        child: Image.asset(
+          background ?? "assets/custom/img/layout.png",
+          fit: BoxFit.fill,
+          height: MediaQuery.of(context).size.height * height,
+          width: MediaQuery.of(context).size.width,
+        ),
       ),
       Padding(
         padding: EdgeInsets.only(
--- lib/pages/resume/view.dart
+++ lib/pages/resume/view.dart
@@ -8,19 +8,25 @@ import 'index.dart';
 class ResumePage extends GetView<ResumeController> {
   const ResumePage({Key? key}) : super(key: key);
 
-  Widget _buildView(BuildContext? _context) {
+  Widget _buildView(BuildContext? _context, {height}) {
     return ContentLayoutWidget(
+        height: height,
         background: "assets/custom/img/resume.png",
-        child: ResumeWidget(),
+        child: const ResumeWidget(),
         text: "");
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async => false,
-        child: Scaffold(
-          body: _buildView(context),
-        ));
+      onWillPop: () async => false,
+      child: Scaffold(
+          body: SingleChildScrollView(
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 2,
+            child: _buildView(context, height: 2)),
+      )),
+    );
   }
 }
