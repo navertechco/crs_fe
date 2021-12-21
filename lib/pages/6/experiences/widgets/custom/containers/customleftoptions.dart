04289659f6921905c185f272f5ab8a0188052fcd
José Cuevas NL <naver.developer@gmail.com>
2021-12-21 12:34:58 -0500

    partial filter

--- lib/pages/6/experiences/widgets/custom/containers/customleftoptions.dart
+++ lib/pages/6/experiences/widgets/custom/containers/customleftoptions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../index.dart';
 
@@ -9,6 +10,9 @@ class CustomLeftOptionsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    RxMap<dynamic, dynamic> localdata =
+        globalctx.context.value["destinationdata"];
+
     return Padding(
       padding: EdgeInsets.only(
           top: MediaQuery.of(context).size.height * 0.3,
@@ -49,6 +53,40 @@ class CustomLeftOptionsWidget extends StatelessWidget {
                         {"code": "2", "description": "Leisure Time"},
                         {"code": "3", "description": "Foods Included"},
                         {"code": "4", "description": "Open Credit"},
+                      ],
+                    ),
+                    CustomFormMultiDropDownFieldWidget(
+                      value: [
+                        {"code": "1", "description": "Adventure"}
+                      ],
+                      onSaved: (value) {},
+                      onChanged: (value) {
+                        // setData(localdata[destination],"key_activities", value);
+                      },
+                      hintText: "Key Activities            ",
+                      data: const [
+                        {"code": "1", "description": "Adventure"},
+                        {"code": "2", "description": "Culture"},
+                        {"code": "3", "description": "History"},
+                        {"code": "4", "description": "Education"},
+                        {"code": "5", "description": "Culinary"},
+                        {"code": "6", "description": "Nature"},
+                        {"code": "7", "description": "Wellness"},
+                        {"code": "8", "description": "Surprise"},
+                        {"code": "9", "description": "Romance"},
+                        // {"code": "2", "description": "hola 2"},
+                      ],
+                    ),
+                    CustomFormDropDownFieldWidget(
+                      // label: "Exploration Days",
+                      onSaved: (value) {},
+                      onChanged: (value) {},
+                      hintText: "Travel Rithm",
+                      data: [
+                        {"code": "1", "description": "Soft"},
+                        {"code": "2", "description": "Medium"},
+                        {"code": "3", "description": "Hard"},
+                        // {"code": "2", "description": "hola 2"},
                       ],
                     ),
                   ],
