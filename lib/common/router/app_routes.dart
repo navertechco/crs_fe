c690d8678d86f2e95871dac64b808f04f33929f2
José Cuevas NL <naver.developer@gmail.com>
2022-02-22 16:39:27 -0500

    stable fade in transition

--- lib/common/router/app_routes.dart
+++ lib/common/router/app_routes.dart
@@ -12,56 +12,56 @@ List<dynamic> routes = [
   PRegistry(
       name: '/Splash',
       page: () => const SplashPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: SplashBinding()),
   PRegistry(
       name: '/Signin',
       page: () => const SigninPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: SigninBinding()),
   PRegistry(
       name: '/Forgot',
       page: () => const ForgotPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: ForgotBinding()),
   PRegistry(
       name: '/Loading',
       page: () => const LoadingPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: LoadingBinding()),
   PRegistry(
       name: '/Tour',
       page: () => const TourPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: TourBinding()),
   PRegistry(
       name: '/Logistic',
       page: () => const LogisticPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: LogisticBinding()),
   PRegistry(
       name: '/Customer',
       page: () => const CustomerPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: CustomerBinding()),
   PRegistry(
       name: '/Destination',
       page: () => const DestinationPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: DestinationBinding()),
   PRegistry(
       name: '/Experiences',
       page: () => const ExperiencesPage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: ExperiencesBinding()),
   PRegistry(
       name: '/Resume',
       page: () => ResumePage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: ResumeBinding()),
   PRegistry(
       name: '/NetRate',
       page: () => NetRatePage(),
-      transition: Transition.native,
+      transition: Transition.rightToLeftWithFade,
       binding: NetRateBinding()),
 ];
