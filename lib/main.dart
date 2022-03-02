a79244f517f686eec30f67c7e1a362a3f6fcc873
José Cuevas NL <naver.developer@gmail.com>
2022-03-02 17:22:30 -0500

    partial customer form travel code

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() {
   var pages = AppPages.pages;
   PRegistry registry =
       routes.toList().firstWhere((element) => element.name == "/Splash");
-
+  
   runApp(GetMaterialApp(
       scrollBehavior: ScrollBehavior(),
       debugShowCheckedModeBanner: false,
