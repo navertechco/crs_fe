ff37ca47b79567cc46a1a0819c61834343f01296
José Cuevas NL <naver.developer@gmail.com>
2022-03-02 09:44:53 -0500

    switch tour

--- lib/pages/2/loading/widgets/loading.dart
+++ lib/pages/2/loading/widgets/loading.dart
@@ -11,6 +11,7 @@ class LoadingWidget extends GetView<LoadingController> {
     required this.ctrl,
   }) : super(key: key);
   LoadingController ctrl;
+  RxBool newtour = false.obs;
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -22,23 +23,63 @@ class LoadingWidget extends GetView<LoadingController> {
               Padding(
                   padding: EdgeInsets.only(
                       left: MediaQuery.of(context).size.width * 0.4,
-                      top: MediaQuery.of(context).size.height * 0.62),
-                  child: RoundedFormField(
-                      onChanged: (value) => ctrl.state.tourId = value!,
-                      hintText: 'Search Quote',
-                      keyboardType: TextInputType.number)),
-              Padding(
-                padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width * 0.4,
-                    top: MediaQuery.of(context).size.height * 0.7),
-                child: RoundedFormButton(
-                    label: "Go",
-                    height: 0.07,
-                    fontSize: 7,
-                    onTap: () {
-                      ctrl.getTour(context, tourId: int.parse(ctrl.state.tourId));
-                    }),
-              ),
+                      top: MediaQuery.of(context).size.height * 0.7),
+                  child: Obx(() {
+                    return Column(
+                      children: [
+                        if (newtour.value)
+                          RoundedFormButton(
+                              label: "New Tour",
+                              height: 0.07,
+                              fontSize: 5,
+                              onTap: () {
+                                Get.toNamed("/Tour");
+                              }),
+                        if (newtour.value)
+                          SizedBox(
+                            child: TextButton(
+                              onPressed: () => newtour.value = !newtour.value,
+                              child: const Text(
+                                "Find tour?",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        if (!newtour.value)
+                          RoundedFormField(
+                              onChanged: (value) => ctrl.state.tourId = value!,
+                              hintText: 'Search Quote',
+                              keyboardType: TextInputType.number),
+                        if (!newtour.value)
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                        if (!newtour.value)
+                          RoundedFormButton(
+                              label: "Find Tour",
+                              height: 0.07,
+                              fontSize: 5,
+                              onTap: () {
+                                ctrl.getTour(context,
+                                    tourId: int.parse(ctrl.state.tourId));
+                              }),
+                        if (!newtour.value)
+                          SizedBox(
+                            child: TextButton(
+                              onPressed: () => newtour.value = !newtour.value,
+                              child: const Text(
+                                "New tour?",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                      ],
+                    );
+                  })),
             ],
           )),
     );
