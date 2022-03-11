6ed8e6f29a2ef5ccdef8b20966997bcddcd593ca
José Cuevas NL <naver.developer@gmail.com>
2022-03-11 17:10:06 -0500

    saferender

--- lib/pages/7/resume/widgets/customformday.dart
+++ lib/pages/7/resume/widgets/customformday.dart
@@ -16,44 +16,46 @@ class CustomFormDayWidget extends StatelessWidget {
   final List<int> indexes;
   @override
   Widget build(BuildContext context) {
-    var destinationindex = indexes[0];
-    var dayindex = indexes[1];
-    var destinations = getCombinedDestinations();
-    var destination = destinations[destinationindex.toString()];
-    var daysData = destination["daysData"];
-    var days = daysData.entries.toList();
-    var day = days[dayindex].value;
-    var meals = day['meals'];
-    var observation = day['observation'];
-    var daydescription = day['day_description'];
-    var dayLabel =
-        (destinationindex != 0 ? destinationindex + 1 : destinationindex) +
-            (destinationindex == 0 ? dayindex + 1 : dayindex + 2);
-    // if (destination == "departure") {
-    //   dayLabel = totalDays.value;
-    // }
-    return RepaintBoundary(
-        key: globalctx.keys["day-$destinationindex-$dayindex"],
-        child: Column(
-          children: [
-            CustomFormTitleWidget(level: 4, label: "Day: $dayLabel#####"),
-            CustomDescriptionWidget(
-                text: daydescription, width: 0.55, fontSize: 0.016),
-            CustomFormExperiencesDetailWidget(data: data, indexes: indexes),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            if (observation != null)
+    try {
+      var destinationindex = indexes[0];
+      var dayindex = indexes[1];
+      var destinations = getCombinedDestinations();
+      var destination = destinations[destinationindex.toString()];
+      var daysData = destination["daysData"] ?? {};
+      var days = daysData.entries.toList();
+      var day = days[dayindex];
+      var meals = day['meals'];
+      var observation = day['observation'];
+      var daydescription = day['day_description'];
+      var dayLabel =
+          (destinationindex != 0 ? destinationindex + 1 : destinationindex) +
+              (destinationindex == 0 ? dayindex + 1 : dayindex + 2);
+
+      return RepaintBoundary(
+          key: globalctx.keys["day-$destinationindex-$dayindex"],
+          child: Column(
+            children: [
+              CustomFormTitleWidget(level: 4, label: "Day: $dayLabel#####"),
               CustomDescriptionWidget(
-                text: "Observation: $observation",
-                width: 0.55,
-                fontSize: 0.016,
-                fontWeight: FontWeight.bold,
-              ),
-            CustomDescriptionWidget(
-                text: "Meals: $meals",
-                width: 0.55,
-                fontSize: 0.016,
-                fontWeight: FontWeight.bold),
-          ],
-        ));
+                  text: daydescription, width: 0.55, fontSize: 0.016),
+              CustomFormExperiencesDetailWidget(data: data, indexes: indexes),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+              if (observation != null)
+                CustomDescriptionWidget(
+                  text: "Observation: $observation",
+                  width: 0.55,
+                  fontSize: 0.016,
+                  fontWeight: FontWeight.bold,
+                ),
+              CustomDescriptionWidget(
+                  text: "Meals: $meals",
+                  width: 0.55,
+                  fontSize: 0.016,
+                  fontWeight: FontWeight.bold),
+            ],
+          ));
+    } catch (e) {
+      return Text("ERROR: CustomFormDayWidget: $e");
+    }
   }
 }
