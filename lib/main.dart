23db7cc3c53ac823b05cbdbaa76dc54d01d9a6db
José Cuevas NL <naver.developer@gmail.com>
2022-04-12 14:07:26 -0500

    ensure main

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,21 @@ void main() {
     WidgetsFlutterBinding.ensureInitialized();
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+    var pages = AppPages.pages;
+    PRegistry registry =
+        routes.toList().firstWhere((element) => element.name == "/Splash");
+
+    runApp(GetMaterialApp(
+        scrollBehavior: ScrollBehavior(),
+        debugShowCheckedModeBanner: false,
+        initialRoute: registry.name,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        defaultTransition: registry.transition,
+        initialBinding: registry.binding,
+        getPages: pages.toList()));
   } catch (e) {
     log(e);
   }
-
-  var pages = AppPages.pages;
-  PRegistry registry =
-      routes.toList().firstWhere((element) => element.name == "/Splash");
-
-  runApp(GetMaterialApp(
-      scrollBehavior: ScrollBehavior(),
-      debugShowCheckedModeBanner: false,
-      initialRoute: registry.name,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      defaultTransition: registry.transition,
-      initialBinding: registry.binding,
-      getPages: pages.toList()));
 }
