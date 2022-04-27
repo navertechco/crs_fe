7d77133c0ce8bdc98baeb1755cb127bab40ac192
José Cuevas NL <naver.developer@gmail.com>
2022-04-27 16:46:46 -0500

    ddns

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -32,7 +32,7 @@ class KTextSytle {
 }
 
 const kDefaultSchema = "https";
-const kDefaultServer = "192.168.101.4"; //"uiodesign.fortiddns.com";
+const kDefaultServer = "uiodesign.fortiddns.com"; //"uiodesign.fortiddns.com";
 const kDefaultServerPort = 9999;
 const kDefaultLogoutPath = "/User/Logout";
 const kDefaultCatalogPath = "/System/FindCatalog";
