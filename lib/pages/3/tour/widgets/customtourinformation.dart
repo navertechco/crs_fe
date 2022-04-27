82599c1c9692e8fe2ad0c17b55b41d217758f207
José Cuevas NL <naver.developer@gmail.com>
2022-04-27 15:14:42 -0500

    fix tour next button

--- lib/pages/3/tour/widgets/customtourinformation.dart
+++ lib/pages/3/tour/widgets/customtourinformation.dart
@@ -30,99 +30,104 @@ class CustomTourInformationForm extends StatelessWidget {
           left: MediaQuery.of(context).size.width * 0.54,
         ),
         child: SizedBox(
-          child: Column(children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            const CustomTitleWidget(
-                width: 0.225,
-                fontWeight: FontWeight.bold,
-                label: "  Tour information"),
-            CustomFormDropDownFieldWidget(
-              value: destCountry.value,
-              disabled: readonly,
-              validator: CustomRequiredValidator(
-                  errorText: "country is required ", ctx: context),
-              onSaved: (value) {
-                ctrl!.state.country = value!;
-                destCountry.value = value;
-                updateDestinationsCatalog();
-              },
-              onChanged: (value) {
-                ctrl!.state.country = value!;
-                destCountry.value = value;
-                updateDestinationsCatalog();
-              },
-              label: "Destination Country ",
-              data: destinationCountry,
-            ),
-            CustomFormDropDownFieldWidget(
-              value: getValue(tour, "accomodation_type", def: "1"),
-              disabled: readonly,
-              onSaved: (value) {
-                ctrl!.state.accomodation_type = value!;
-              },
-              onChanged: (value) {
-                ctrl!.state.accomodation_type = value!;
-              },
-              validator: CustomRequiredValidator(
-                  errorText: "Accomodation type is required ", ctx: context),
-              label: "Accomodation Type",
-              data: accomodationType,
-            ),
-            CustomFormTextFieldWidget(
-                disabled: readonly,
-                value: getValue(tour, "passengers", def: "10"),
-                validator: CustomRequiredValidator(
-                    errorText: "Passengers is required ", ctx: context),
-                onSaved: (value) {
-                  ctrl!.state.passengers = value!;
-                },
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ],
-                label: "Passengers                  ",
-                width: 0.20),
-            Obx(() {
-              return CustomFormMultiDropDownFieldWidget(
-                validator: (value) => CustomMultiDropdownRequiredValidator(
-                    value,
-                    errorText: "Purposes are required ",
-                    context: context),
-                value: purposeMemory.value,
-                onSaved: (values) {
-                  savePurposes(values);
-                },
-                onChanged: (values) {
-                  savePurposes(values);
-                },
-                hintText: " ",
-                label: "Purposes                      ",
-                data: purposes,
-              );
-            }),
-            Padding(
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.17,
-                left: MediaQuery.of(context).size.width * 0.022,
-              ),
-              child: CustomKeypadWidget(
-                  width: 0.265,
-                  onPrevious: () {
-                    if (selectedIndex.value > 0) {
-                      selectedIndex.value = selectedIndex.value - 1;
-                    }
-
-                    Get.back();
+          child: Stack(
+            children: [
+              Column(children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                const CustomTitleWidget(
+                    width: 0.225,
+                    fontWeight: FontWeight.bold,
+                    label: "  Tour information"),
+                CustomFormDropDownFieldWidget(
+                  value: destCountry.value,
+                  disabled: readonly,
+                  validator: CustomRequiredValidator(
+                      errorText: "country is required ", ctx: context),
+                  onSaved: (value) {
+                    ctrl!.state.country = value!;
+                    destCountry.value = value;
+                    updateDestinationsCatalog();
                   },
-                  onNext: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      ctrl!.sendTour();
-                      selectedIndex.value = selectedIndex.value + 1;
-                    }
-                  }),
-            ),
-          ]),
+                  onChanged: (value) {
+                    ctrl!.state.country = value!;
+                    destCountry.value = value;
+                    updateDestinationsCatalog();
+                  },
+                  label: "Destination Country ",
+                  data: destinationCountry,
+                ),
+                CustomFormDropDownFieldWidget(
+                  value: getValue(tour, "accomodation_type", def: "1"),
+                  disabled: readonly,
+                  onSaved: (value) {
+                    ctrl!.state.accomodation_type = value!;
+                  },
+                  onChanged: (value) {
+                    ctrl!.state.accomodation_type = value!;
+                  },
+                  validator: CustomRequiredValidator(
+                      errorText: "Accomodation type is required ",
+                      ctx: context),
+                  label: "Accomodation Type",
+                  data: accomodationType,
+                ),
+                CustomFormTextFieldWidget(
+                    disabled: readonly,
+                    value: getValue(tour, "passengers", def: "10"),
+                    validator: CustomRequiredValidator(
+                        errorText: "Passengers is required ", ctx: context),
+                    onSaved: (value) {
+                      ctrl!.state.passengers = value!;
+                    },
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    label: "Passengers                  ",
+                    width: 0.20),
+                Obx(() {
+                  return CustomFormMultiDropDownFieldWidget(
+                    validator: (value) => CustomMultiDropdownRequiredValidator(
+                        value,
+                        errorText: "Purposes are required ",
+                        context: context),
+                    value: purposeMemory.value,
+                    onSaved: (values) {
+                      savePurposes(values);
+                    },
+                    onChanged: (values) {
+                      savePurposes(values);
+                    },
+                    hintText: " ",
+                    label: "Purposes                      ",
+                    data: purposes,
+                  );
+                }),
+              ]),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.635,
+                  left: MediaQuery.of(context).size.width * 0.022,
+                ),
+                child: CustomKeypadWidget(
+                    width: 0.265,
+                    onPrevious: () {
+                      if (selectedIndex.value > 0) {
+                        selectedIndex.value = selectedIndex.value - 1;
+                      }
+
+                      Get.back();
+                    },
+                    onNext: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        ctrl!.sendTour();
+                        selectedIndex.value = selectedIndex.value + 1;
+                      }
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
