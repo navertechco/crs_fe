b1697458c6fe62ef9e0555a388b8a02042b09863
José Cuevas NL <naver.developer@gmail.com>
2022-05-26 09:55:30 -0500

    fix destination keypad aligment

--- lib/pages/5/destination/widgets/custom/customrightstardestination.dart
+++ lib/pages/5/destination/widgets/custom/customrightstardestination.dart
@@ -11,34 +11,44 @@ class CustomRightStarDestinationForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.055,
+                left: MediaQuery.of(context).size.width * 0.5,
+              ),
+              child: DragDestinationWidget(
+                  destination: arrival["description"],
+                  index: 0,
+                  type: "arrival",
+                  out: true),
+            ),
+            DestinationsOrderableListWidget(),
+            Obx(() {
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.0,
+                  left: MediaQuery.of(context).size.width * 0.5,
+                ),
+                child: DragDestinationWidget(
+                    destination: departure["description"],
+                    index: globalctx.destinationDragData.value.length - 1,
+                    type: "departure",
+                    out: true),
+              );
+            }),
+          ],
+        ),
         Padding(
           padding: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.055,
-            left: MediaQuery.of(context).size.width * 0.5,
+            top: MediaQuery.of(context).size.height * 0.86,
+            left: MediaQuery.of(context).size.width * 0.0,
           ),
-          child: DragDestinationWidget(
-              destination: arrival["description"],
-              index: 0,
-              type: "arrival",
-              out: true),
+          child: const KeyPadWidget(),
         ),
-        DestinationsOrderableListWidget(),
-        Obx(() {
-          return Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.0,
-              left: MediaQuery.of(context).size.width * 0.5,
-            ),
-            child: DragDestinationWidget(
-                destination: departure["description"],
-                index: globalctx.destinationDragData.value.length - 1,
-                type: "departure",
-                out: true),
-          );
-        }),
-        const KeyPadWidget(),
       ],
     );
   }
