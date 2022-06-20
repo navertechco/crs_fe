44bf68224be1d72d083f641509162294f3902065
José Cuevas NL <naver.developer@gmail.com>
2022-06-20 09:07:22 -0500

    shelf

---
+++ server.dart
@@ -0,0 +1,20 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:path/path.dart' as p;
+
+void main() async {
+  var staticPath = 'build/web';
+  var staticHandler = createStaticHandler(staticPath,
+      defaultDocument: 'index.html', serveFilesOutsidePath: true);
+  var mainHandler = createStaticHandler(staticPath,
+      defaultDocument: 'main.dart.js', serveFilesOutsidePath: true);
+  var assetsHandler = createFileHandler('build\\web\\assets');
+  var app = Router();
+  app.get('/', staticHandler);
+  app.get('/main.dart.js', mainHandler);
+  app.get('/assets/', assetsHandler);
+  var server = await io.serve(app, 'localhost', 7777);
+  print('Serving at http://${server.address.host}:${server.port}');
+}
